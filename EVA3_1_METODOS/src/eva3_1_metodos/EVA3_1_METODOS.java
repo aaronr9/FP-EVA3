c332c106dfc13248a3c25e4023ab75822c9e9315
aaronr9 <L24550244@chihuahua2.tecnm.mx>
2024-11-07 21:46:29 -0600

    Add files via upload

---
+++ EVA3_1_METODOS/src/eva3_1_metodos/EVA3_1_METODOS.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva3_1_metodos;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA3_1_METODOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //INVOCAR EL METODO. LLAMADA A METODO --> USARLO
+        imprimirMensaje("HOLA MUNDO!!");
+     
+    }
+    //declaracion del metodo
+    //MODIFICACIONES (OPCIONALES)
+       //VALOR RETORNO
+           //NOMBRE
+                      //PARAMETROS (OPCIONALES)
+    //SON UTILES PARA REUTILIZAR CODIGO
+    public static void imprimirMensaje(String mensaje){
+        //cuerpo del metodo
+        System.out.println("Tu mensaje: " + mensaje);
+        
+    }
+    
+}
